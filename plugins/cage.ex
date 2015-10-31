a3ddf7ada38cb271eb4680dba80311844c782c6b
Chris Keathley <c@keathley.io>
2015-10-31 00:39:34 -0400

    Seed random numbers

--- plugins/cage.ex
+++ plugins/cage.ex
@@ -4,6 +4,7 @@ defmodule ChadevBot.Cage do
   @url "http://cageme.herokuapp.com"
 
   respond ~r/cageme/, conn do
+    :random.seed(:os.timestamp)
     random = :random.uniform(80)
     reply conn, "#{@url}/specific/#{random}.jpeg"
   end
