437d28fd878217deeb535673b011be32a4b231a6
Chris Keathley <c@keathley.io>
2015-11-01 19:55:45 -0500

    Update butler version

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule ChadevBot.Mixfile do
   defp deps do
     [
       {:websocket_client, git: "http://github.com/jeremyong/websocket_client"},
-      {:butler, "~> 0.6.0"}
+      {:butler, "~> 0.6.1"}
     ]
   end
 end
